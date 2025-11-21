830012715522f791978013a0adf7afff7c4ff219
Jeroen De Meester <j.de.meester@rijksmuseum.nl>
2025-11-21 17:24:58 +0100

    .env setup en testscript

--- README.md
+++ README.md
@@ -2,14 +2,3 @@
 A Python script for batch adding Rijksmuseum URIs to Wikidata using the OAuth API.
 
 # my first steps...
-
-Create a `.env` file in the root directory:
-
-   ```env
-   CONSUMER_KEY=your_consumer_key
-   CONSUMER_SECRET=your_consumer_secret
-   ACCESS_TOKEN=your_access_token
-   ACCESS_SECRET=your_access_secret
-   ```
-
-   > **Note:** Never commit this file. `.env` is included in `.gitignore`.
---
+++ test.py
@@ -0,0 +1,15 @@
+# Importeer de benodigde modules
+from dotenv import load_dotenv # Om variabelen uit een .env bestand te laden
+import os # Om toegang te krijgen tot systeem- en omgevingsvariabelen
+
+# Laad het .env bestand. Hierdoor worden alle variabellen uit .env beschikbaar
+load_dotenv()
+
+# Haal waarden uit .env (Wikidata API OAuth 1.0 keys)
+CONSUMER_KEY = os.getenv("CONSUMER_KEY")
+CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_SECRET = os.getenv("ACCESS_SECRET")
+
+# print key as test
+print(CONSUMER_KEY)
